8b7c7af87d32c54031f40b10915cff4a5c735663
PetrosaMan <dw.todd.eng@gmail.com>
2024-11-20 23:03:46 +0200

    Complete rewrite script.js code

--- script.js
+++ script.js
@@ -1,70 +1,53 @@
 "use strict" 
 
-function Calculator(containerId) {
+function Calculator(container) {
 
-    this.operandOne = "";
-    this.operandTwo = "";
-    this.operator = "";
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operator = "";    
     this.display = "0";
-    this.target;
 
-    this.container = document.querySelector(`#${containerId}`);
-    
-   // add event listener to the container
-    this.container.addEventListener('click',(event) => {       
-        let target = event.target;      
+    this.allClear = function() {
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.display = "0";
+        this.operator = "";
+    }
 
-        switch(target.value) {
-            case '.':
-                if(this.display.includes('.')) {
-                    return;
-                } 
-                this.display += '.';
-                break;
-            case '0':                                  
-                if (this.display === '0') {
-                    this.display = "0";                                           
-                } else {
-                    this.display += '0'
-                } 
+    this.add = function(a, b) {        
+        return parseFloat(a) + parseFloat(b);
+    }
 
-                break;
-            case '1':
-                this.display += '1';                
-                break;
-            case '2':                 
-                this.display += '2';               
-                break; 
-            case '3':                 
-                this.display += '3';               
-                break; 
-            case '4':                 
-                this.display += '4';               
-                break; 
-            case '5':                 
-                this.display += '5';               
-                break; 
-            case '6':                 
-                this.display += '6';               
-                break; 
-            case '7':                 
-                this.display += '7';               
-                break; 
-            case '8':                 
-                this.display += '8';               
-                break; 
-            case '9':                 
-                this.display += '9';               
-                break;                                 
-            default:
-                console.log('bad input');
-                break;
-        }         
-            this.display = this.display.replace(/^0+(?=\d)/, '');
-        console.log("this.display ", this.display);
-    });
+    this.subtract = function(a, b) {
+        return parseFloat(a) - parseFloat(b);
+    }    
 
+    this.multiply = function(a, b) {
+        return parseFloat(a) * parseFloat(b);
+    }
+
+    this.divide = function(a, b) {
+        return parseFloat(a) / parseFloat(b);
+    }
+
+    this.changeSign = function(a) {
+        return parseFloat(-a);
+    }
+
+    this.percentage = function(a) {
+        return parseFloat(a) / 100;
+    }
+   
 }
-
 // Create an instance of the calculator
 let calc = new Calculator('container');
+
+console.log(calc.add(2, 4));
+console.log(calc.subtract(2, 4));
+console.log(calc.multiply(2, 4));
+console.log(calc.divide(2, 4));
+console.log(calc.changeSign(7));
+console.log(calc.changeSign(-8));
+console.log(calc.percentage(20));
+
+
