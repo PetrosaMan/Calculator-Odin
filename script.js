213114faf6dcaa33eba1a7e10e9f5c91be1baed2
PetrosaMan <dw.todd.eng@gmail.com>
2024-11-21 15:51:31 +0200

    many code changes to script.js major rethink on code logic

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict" 
 
-function Calculator(container) {
+function Calculator() {
 
     this.currentOperand = "";
     this.previousOperand = "";
     this.operator = "";    
     this.display = "0";
+    this.decimal = "";
 
     this.allClear = function() {
         this.currentOperand = "";
@@ -37,17 +38,57 @@ function Calculator(container) {
     this.percentage = function(a) {
         return parseFloat(a) / 100;
     }
+
+    let disp = document.querySelector("#display");
+    console.log(disp);
+    
+    this.updateDisplay = function() {        
+        this.display = this.currentOperand;
+        console.log("display ", this.display);
+        //disp.value = this.currentOperand;
+    }
+
+    this.appendNumber = function(value) {        
+        this.currentOperand += value;
+        this.currentOperand.replace(/^0+(?=\d)/, "");
+        this.updateDisplay();        
+    } 
+    
+    this.appendDecimal = function() {        
+        if(this.currentOperand.includes(".")) {
+            return;
+        } else {
+            this.appendNumber(".");
+        }
+    }
+
+    
+
+    let container = document.querySelector("#container");
+    
+    
+    container.addEventListener("click", (e) => {
+        let target =e.target;
+        switch(target.value){
+            case "allClear":
+                this.allClear();                
+                break;
+            case  "7":
+                this.appendNumber("7");
+                break;   
+        }
+    });
    
 }
 // Create an instance of the calculator
-let calc = new Calculator('container');
+let calc = new Calculator();
 
-console.log(calc.add(2, 4));
-console.log(calc.subtract(2, 4));
-console.log(calc.multiply(2, 4));
-console.log(calc.divide(2, 4));
-console.log(calc.changeSign(7));
-console.log(calc.changeSign(-8));
-console.log(calc.percentage(20));
+//console.log(calc.add(2, 4));
+//console.log(calc.subtract(2, 4));
+//console.log(calc.multiply(2, 4));
+//console.log(calc.divide(2, 4));
+//console.log(calc.changeSign(7));
+//console.log(calc.changeSign(-8));
+//console.log(calc.percentage(20));
 
 
