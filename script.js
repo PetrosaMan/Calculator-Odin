e3d9aaa6cc19ba6276a2d360c4980cb5df2344d0
PetrosaMan <dw.todd.eng@gmail.com>
2024-11-21 22:52:10 +0200

    added change sign function and some changes to append functions

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ function Calculator() {
     this.currentOperand = "";
     this.previousOperand = "";
     this.operator = "";    
-    this.display = "0";
+    this.display = "";
     this.decimal = "";
 
-    this.allClear = function() {
+    this.allClear = function() {        
         this.currentOperand = "";
         this.previousOperand = "";
-        this.display = "0";
+        this.display = "";
         this.operator = "";
+        document.querySelector("#display").textContent = 0;
     }
 
     this.add = function(a, b) {        
@@ -31,41 +32,51 @@ function Calculator() {
         return parseFloat(a) / parseFloat(b);
     }
 
-    this.changeSign = function(a) {
-        return parseFloat(-a);
+    this.changeSign = function() {
+        console.log("change sign function");
+        let temp = parseFloat(this.currentOperand);
+        temp = -temp;
+        console.log("temp: ", temp);
+        this.currentOperand = temp;
+        console.log(this.currentOperand);
+        this.updateDisplay();
+        return;
     }
 
     this.percentage = function(a) {
         return parseFloat(a) / 100;
     }
-
-    let disp = document.querySelector("#display");
-    console.log(disp);
     
     this.updateDisplay = function() {        
         this.display = this.currentOperand;
-        console.log("display ", this.display);
-        //disp.value = this.currentOperand;
+        document.querySelector("#display").textContent = this.display;
+        return;       
     }
 
-    this.appendNumber = function(value) {        
-        this.currentOperand += value;
-        this.currentOperand.replace(/^0+(?=\d)/, "");
+    this.appendNumber = function(value) {  
+        if(this.currentOperand === 0 && value !== ".") {
+            this.currentOperand = value; //Replace leading zero
+        } else {
+            this.currentOperand += value; // Append value
+        }
+        
+        // Remove leading zeros for integers but keep "0." for decimals
+        if(!this.currentOperand.includes(".")) {
+            this.currentOperand = this.currentOperand.replace(/^0+(?=\d)/, "");
+        }            
         this.updateDisplay();        
     } 
-    
-    this.appendDecimal = function() {        
-        if(this.currentOperand.includes(".")) {
-            return;
-        } else {
-            this.appendNumber(".");
+        
+    this.appendDecimal = function() {
+        if(this.currentOperand === "") {
+            this.currentOperand ="0."; // start with "0." if empty
+        } else if(!this.currentOperand.includes(".")) {
+            this.currentOperand += ".";  // Appends decimal point
         }
-    }
-
+        this.updateDisplay();
+    }   
     
-
-    let container = document.querySelector("#container");
-    
+    let container = document.querySelector("#container");    
     
     container.addEventListener("click", (e) => {
         let target =e.target;
@@ -73,9 +84,42 @@ function Calculator() {
             case "allClear":
                 this.allClear();                
                 break;
+            case  "9":
+                this.appendNumber("9");
+                break; 
+            case  "8":
+                this.appendNumber("8");
+                break; 
             case  "7":
                 this.appendNumber("7");
-                break;   
+                break; 
+            case  "6":
+                this.appendNumber("6");
+                break;
+            case  "5":
+                this.appendNumber("5");
+                break;
+            case  "4":
+                this.appendNumber("4");
+                break;
+            case  "3":
+                this.appendNumber("3");
+                break; 
+            case  "2":
+                this.appendNumber("2");
+                break; 
+            case  "1":
+                this.appendNumber("1");
+                break;
+            case  "0":
+                this.appendNumber("0");
+                break; 
+            case ".":
+                this.appendDecimal("."); 
+                break; 
+            case "sign":
+                this.changeSign();
+                break;                  
         }
     });
    
